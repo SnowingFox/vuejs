57a55bc1f6f0788b2daf0c6a8bcf4e38c37feeb2
snowingfox <1503401882@qq.com>
2022-04-25 15:17:36 +0800

    feat: add isReadonly

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,12 +1,22 @@
-import { Target } from './reactive'
+import { ReactiveFlags, Target } from './reactive'
 import { track, trigger } from './effect'
 
 const get = /*#__PURE__*/ createGetter()
+const readonlyGet = /*#__PURE__*/ createGetter(true)
+
 const set = /*#__PURE__*/ createSetter()
 
-function createGetter(isReadonly = false): ProxyHandler<any>['get'] {
+function createGetter(isReadonly = false, isShallow = false): ProxyHandler<any>['get'] {
   return function get(target: Target, key: string | symbol, receiver: object) {
     const res = Reflect.get(target, key, receiver)
+
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    } else if (key === ReactiveFlags.IS_SHALLOW) {
+      return isShallow
+    }
 
     if (!isReadonly) {
       track(target, key)
@@ -30,4 +40,16 @@ function createSetter(): ProxyHandler<any>['set'] {
 export const mutableHandlers: ProxyHandler<object> = {
   get,
   set,
+}
+
+export const readonlyHandlers: ProxyHandler<object> = {
+  get: readonlyGet,
+  set(target, key) {
+    console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`)
+    return true
+  },
+  defineProperty(target, key) {
+    console.warn(`DefineProperty operation on key "${String(key)}" failed: target is readonly.`)
+    return true
+  },
 }
