17e6ca1bc41dfc07456accd3652350f954dcfdbf
snowingfox <1503401882@qq.com>
2022-04-27 19:20:42 +0800

    test: ref & isRef

---
+++ packages/reactivity/__test__/ref.spec.ts
@@ -0,0 +1,62 @@
+import { isRef, ref } from '../src/ref'
+import { effect } from '../src'
+
+describe('reactivity/ref', () => {
+  it('should hold a value', () => {
+    const a = ref(1)
+    expect(a.value).toBe(1)
+    a.value = 2
+    expect(a.value).toBe(2)
+  })
+
+  it('should be reactive', () => {
+    const a = ref(1)
+    let dummy
+    let calls = 0
+    effect(() => {
+      calls++
+      dummy = a.value
+    })
+    expect(calls).toBe(1)
+    expect(dummy).toBe(1)
+    a.value = 2
+    expect(calls).toBe(2)
+    expect(dummy).toBe(2)
+    // same value should not trigger
+    a.value = 2
+    expect(calls).toBe(2)
+  })
+
+  it('should make nested properties reactive', () => {
+    const a = ref({
+      count: 1,
+    })
+    let dummy
+    effect(() => {
+      dummy = a.value.count
+    })
+    expect(dummy).toBe(1)
+    a.value.count = 2
+    expect(dummy).toBe(2)
+  })
+
+  it('should work without initial value', () => {
+    const a = ref()
+    let dummy
+    effect(() => {
+      dummy = a.value
+    })
+    expect(dummy).toBe(undefined)
+    a.value = 2
+    expect(dummy).toBe(2)
+    a.value = 3
+    expect(dummy).toBe(3)
+  })
+
+  test('isRef', () => {
+    const original = { foo: 1 }
+    const obj = ref(original)
+    expect(isRef(obj)).toBe(true)
+    expect(isRef(original)).toBe(false)
+  })
+})
