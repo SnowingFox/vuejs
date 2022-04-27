968289c1a42395c51a19d11e81efac5eeb7e2bde
snowingfox <1503401882@qq.com>
2022-04-27 19:18:58 +0800

    fix: fix optional sign

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -2,7 +2,7 @@ import { ReactiveEffect } from './effect'
 
 export type Dep = Set<ReactiveEffect>
 
-export const createDep = (effects: ReactiveEffect[]): Dep => {
+export const createDep = (effects?: ReactiveEffect[]): Dep => {
   const dep = new Set<ReactiveEffect>(effects)
 
   return dep
